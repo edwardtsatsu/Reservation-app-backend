44aac8225efffc10b6fb3c8ed04ac7dc829e9351
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2022-02-03 22:06:26 +0000

    Initial commit

---
+++ ReservationProject/src/main/java/com/giskard/ReservationProject/ReservationProjectApplication.java
@@ -0,0 +1,12 @@
+package com.giskard.ReservationProject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationProjectApplication.class, args);
+	}
+
+}
---
+++ ReservationProject/src/test/java/com/giskard/ReservationProject/ReservationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giskard.ReservationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
