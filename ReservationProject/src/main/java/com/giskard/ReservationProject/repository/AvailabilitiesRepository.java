5e02c7c6d2b39d4ddac667eb94869e03b8574337
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2022-02-03 23:25:31 +0000

    worked on the service

--- ReservationProject/src/main/java/com/giskard/ReservationProject/repository/AvailabilitiesRepository.java
+++ ReservationProject/src/main/java/com/giskard/ReservationProject/repository/AvailabilitiesRepository.java
@@ -1,7 +1,10 @@
 package com.giskard.ReservationProject.repository;
-
+import com.giskard.ReservationProject.model.Availabilities;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface AvailabilitiesRepository extends JpaRepository<Appendable,Long> {
 
+    Optional<Availabilities> findAvailabilitiesById(Long id);
 }
--- ReservationProject/src/main/java/com/giskard/ReservationProject/repository/ReservationRepository.java
+++ ReservationProject/src/main/java/com/giskard/ReservationProject/repository/ReservationRepository.java
@@ -1,7 +1,9 @@
 package com.giskard.ReservationProject.repository;
 import com.giskard.ReservationProject.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
 
+    Optional<Reservation> findReservationById(Long id);
 }
